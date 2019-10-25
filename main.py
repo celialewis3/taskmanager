029cb25031774ddd4eb1a3287d28bcd25627d749
Celia <celialewis@umass.edu>
2019-10-25 16:31:57 -0400

    Added functionality to read in file; program remembers incomplete tasks.

--- main.py
+++ main.py
@@ -7,32 +7,46 @@ from taskmanager import TaskManager
 
 
 def main():
-  # Creates taskmanager instance and opens file for output, writes date.
+  # Creates taskmanager instance and opens file for output.
   tm = TaskManager()
 
   # Start program.
+  # Imports task list from file & displays it.
   print("Hi! I'm Herupa, your task helper.")
-  print("Enter your list of tasks to begin.")
-  print("Enter each task at a time. Type Done when you're finished.")
-  tm.makeTaskList()
   tm.displayTasks()
   print()
 
+  # If we have no tasks, create a list.
+  if not tm.hasTasks():
+    tm.makeTaskList()
+    tm.displayTasks()
+
+  # While we have tasks, let the user enter a task, finish a task, or view completed tasks.
   while(tm.hasTasks()):
       print()
+      print("E [task] to enter new task.")
       print("F [task] to finish a task.")
       print("L to see a list of completed tasks.")
+      print("Q to quit.")
       response = input()
       if response[0] == "F" or response[0] == "f":
         cur_task = response[2:]
         tm.finishTask(cur_task)
         tm.displayTasks()
-      else:
+      elif response[0] == "e" or response[0] == "e":
+        cur_task = response[2:]
+        tm.newTask(cur_task)
+        tm.displayTasks()
+      elif response[0] == "q" or response[0] == "Q":
+          break
+      elif response[0] == 'l' or response[0] == "L":
         tm.displayCompletedTasks()
+      else:
+        print("Not a valid command.")
 
   print()
-  print("You've finished every task and earned 50XP!")
   tm.displayCompletedTasks()
+  tm.closeFile()
 
 
 
--- taskmanager.py
+++ taskmanager.py
@@ -11,9 +11,28 @@ class TaskManager:
     completed_tasks = []
 
     def __init__(self):
-        self.fd = open("tasks.txt", "a+")
-        date_nl = datetime.datetime.now().strftime("%m/%d/%Y, %I:%M") + '\n'
-        self.fd.write(date_nl)
+        # When task manager inits, open task file and read in tasks.
+        # Tasks marked completed go to the completed list, and tasks
+        # marked incomplete go to the active task list.
+        self.fd = open("tasks.txt", "r+")
+
+        # write date to file, beginning new entry
+        self.fd.write("d_"+datetime.datetime.now().strftime("%m/%d/%Y %I:%M:%S")+'\n')
+
+        for line in self.fd:
+            line = line[:-1]
+            words = line.split()
+            if words[0][0:2] == "d_":
+                pass
+            elif words[1] == "completed":
+                self.completed_tasks.append([words[0], words[1], words[2]])
+            else:
+                self.task_list.append([words[0], words[1]])
+        self.fd.close()
+        self.fd = open("tasks.txt", "w")
+        self.fd.write("d_" + datetime.datetime.now().strftime("%m/%d/%Y %I:%M:%S") + '\n')
+
+
 
     def makeTaskList(self):
         self.task_list = []
@@ -29,13 +48,8 @@ class TaskManager:
             else:
                 self.task_list.append([task, 'incomplete'])
 
-        # Once complete, write the tasks to the output file.
-        taskString = "tasks: "
-        for x in self.task_list[:-1]:
-            taskString += x[0] + ", "
-        taskString += self.task_list[-1][0] + '\n'
-        self.fd.write(taskString)
-        self.fd.close()
+    def newTask(self, task):
+        self.task_list.append([task, 'incomplete'])
 
     def displayTasks(self):
         if len(self.task_list) > 0:
@@ -54,8 +68,22 @@ class TaskManager:
 
     def finishTask(self, taskName):
         try:
+            # Remove task from active task list.
             self.task_list.remove([taskName, 'incomplete'])
-            taskTime = datetime.datetime.now().strftime("%m/%d/%Y %I:%M:%S")
-            self.completed_tasks.append([taskName, 'complete', taskTime])
+            taskTime = datetime.datetime.now().strftime("%m/%d/%Y_%I:%M:%S")
+            # Add task with completion time to completed task list.
+            self.completed_tasks.append([taskName, 'completed', taskTime])
+            # Write completed task to file.
+            taskString = taskName + " completed " + taskTime + '\n'
+            self.fd.write(taskString)
         except:
             print("That task name isn't on the list, try again.")
+
+    def closeFile(self):
+        # If tasks were remaining at quit time, write them to the file as incomplete.
+        if len(self.task_list) > 0:
+            taskString = ""
+            for entry in self.task_list:
+                taskString = entry[0] + " " + entry[1] + '\n'
+                self.fd.write(taskString)
+        self.fd.close()
